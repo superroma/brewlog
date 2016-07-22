d8bcf09e2ff44e7460a0b1c65797608881121032
Roman Eremin (DevExpress) <eremin@romans-mbp.corp.devexpress.com>
2016-07-22 16:55:16 +0300

    add bootstrap loader no jquery

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,11 +1,14 @@
 var path = require('path');
 var webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+
 
 module.exports = {
   devtool: 'eval',
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
+    'bootstrap-loader',
     './src/index'
   ],
   output: {
@@ -17,10 +20,22 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin()
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
+    loaders: [
+      { test: /\.js$/, loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'src')
+      },
+      { test: /\.css$/, loaders: [ 'style', 'css', 'postcss' ] },
+      { test: /\.scss$/, loaders: [ 'style', 'css', 'postcss', 'sass' ] },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url?limit=10000"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        loader: 'file'
+      },
+      { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports?jQuery=jquery' },
+    ]
+  },
+  postcss: [ autoprefixer ],
 };
