270d241629f6af2fc772786054a37aaf9e89dbcf
Roman Eremin (DevExpress) <eremin@ROMANs-MBP.local>
2016-07-29 18:43:20 +0300

    prepare for hot loader 3

--- server/src/index.js
+++ server/src/index.js
@@ -1,25 +1,5 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {Router, Route, IndexRoute, hashHistory} from 'react-router'
-import App from './App';
-import {BatchesView} from './BatchesView'
-import {
-  Home,
-  InventoryView,
-  SettingsView,
-} from './Stubs'
-import {BatchDetailView} from './BatchDetailView'
+import Root from './Root'
 
-render(
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home}/>
-      <Route path="batches" component={BatchesView}/>
-      <Route path="batchdetails" component={BatchDetailView}/>
-      <Route path="Inventory" component={InventoryView}/>
-      <Route path="Settings" component={SettingsView}/>
-    </Route>
-  </Router>
-  , document.getElementById('root'));
-
-  module.hot.accept();
+render(<Root/>, document.getElementById('root'));
